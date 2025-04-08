0adb8c56160a4b305719800ef2f3a0a6112571f6
vikrantsrgc <145971065+vikrantsrgc@users.noreply.github.com>
2025-04-08 12:06:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//your JS code here. If required.
-let paraEle = document.getElementById("status");
-let enterBtn = document.getElementById("enterBtn");
+// //your JS code here. If required.
+// let paraEle = document.getElementById("status");
+// let enterBtn = document.getElementById("enterBtn");
 
-enterBtn.addEventListener("click",()=>{
-	// create a new h1 elemet 
-	const h1 = document.createElement("h1")
-	h1.textContent = "Entered Metaverse"
+// enterBtn.addEventListener("click",()=>{
+// 	// create a new h1 elemet 
+// 	const h1 = document.createElement("h1")
+// 	h1.textContent = "Entered Metaverse"
 	
-	paraEle.replaceWith(h1)
-})
+// 	paraEle.replaceWith(h1)
+// })
