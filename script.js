2d23d729bc285a1895c26411a867492385b18a2e
vikrantsrgc <145971065+vikrantsrgc@users.noreply.github.com>
2025-04-08 11:44:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ let paraEle = document.getElementById("status");
 let enterBtn = document.getElementById("enterBtn");
 
 enterBtn.addEventListener("click",()=>{
-	paraEle.textContent = "Entered Metaverse"
+	// create a new h1 elemet 
+	const h1 = document.createElement("h1")
+	h1.textContent = "Entered Metaverse"
+	
+	paraEle.replaceWith(h1)
 })
